2f6f237efd8ecafb066ee43848d76c172e46b71f
Thomas V <thomas.vog67@gmail.com>
2023-03-02 20:28:22 +0100

    ajout des fichiers

---
+++ FrontEnd/script.js
@@ -0,0 +1,14 @@
+//fetch ("http://localhost:5678/api/categories")
+//.then (categories => categories.json())
+//.then (categories2 => console.table(categories2));
+
+
+
+//fetch ("http://localhost:5678/api/works")
+//.then (works => works.json())
+//.then (works2=> console.table(works2));
+
+const boutonTous=document.querySelector('.filtre-tous');
+console.log (boutonTous);
+
+
