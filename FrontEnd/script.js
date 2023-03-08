77b7b0ef35111f749f74df9495eebda120498c82
Thomas V <thomas.vog67@gmail.com>
2023-03-08 16:15:26 +0100

    ajout des images dynamiques et debut des filtres

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,59 +1,96 @@
- 
- /*async function listeCategories () {
+ console.log ("connecte !");
+
+ async function listeCategories () {
  let response= await fetch ("http://localhost:5678/api/categories")
- let info = await response.json()
- console.table (info)
- 
+ let infoCategorie = await response.json()
+//infoCategorie.unshift("Tous"); rajoute juste une valeur tous
+/*let tous = {
+    id: 0,
+    name:"tous"                          voulu creer un array tous pour l'inserer dans infoCategorie
+};
+let boutonTous = [tous]; 
+boutonTous.push(infoCategorie);*/
+console.table (infoCategorie);
+
+
+for (let i = 0; i < infoCategorie.length; i++) {
+id = infoCategorie[i].id;
+nom = infoCategorie[i].name;
+affichageBouton(id,nom);
+
+
+
+}
 }
 
-listeCategories()*/
+listeCategories()
 
-async function listeImage () {
 
- let response = await fetch ("http://localhost:5678/api/works")
-let infoImage = await response.json()
-console.table (infoImage);
+
+
+
+
+/*for (let i = 0 ; i < infoImage.length ; i++) {
+    srcValue= infoImage.imageUrl[i]
+    figCaptionValue= infoImage.title[i]
+
+} */
+
+function affichageBouton (id,nom) {
+
+let bouton = document.createElement("bouton");
+let textebouton = document.createElement("texte");
+
+
+bouton.setAttribute("btn",id);
+
+textebouton.append(nom);
+bouton.append(id);
+console.log(textebouton);
+console.log(id);
+
+
 
 }
 
 
-listeImage() 
 
 
+
+
+async function listeProjet() {
+
+    let response = await fetch("http://localhost:5678/api/works")
+    let infoImage = await response.json()
+        //console.table (infoImage);
+    for (let i = 0; i < infoImage.length; i++) {
+        srcValue = infoImage[i].imageUrl;
+        figCaptionValue = infoImage[i].title;
+        affichageProjet(srcValue, figCaptionValue);
+    }
+}
+
+
+listeProjet()
+
+
+function affichageProjet(srcValue, figCaptionValue) {
+
+    let gallery = document.querySelector(".gallery");
+
+    let figure = document.createElement("figure");
+    let img = document.createElement("img");
+    let figCaption = document.createElement("figCaption");
 
    
-
-for (let i = 0 ; i < listeImage.length ; i++) {
-    srcValue= listeImage.imageUrl[i]
-    figCaptionValue= listeImage.title[i]
-
-}
-
-function affichageProjet (srcValue,figCaptionValue) {
-
-let gallery= document.querySelector(".gallery");
-
-let figure= document.createElement("figure");
-let img= document.createElement("img");
-let figCaption= document.createElement("figCaption");
-
-srcValue= listeImage.imageUrl
-img.setAttribute("src",srcValue);
-figCaption.textContent= figCaptionValue;
-gallery.append(figure);
-figure.append(img, figCaption);
+    
+    img.setAttribute("src",srcValue);
+    figCaption.textContent = figCaptionValue;
+    gallery.append(figure);
+    figure.append(img, figCaption);
 
 
 }
-
-affichageProjet()
-
-
-
-
-
-
-
 
 
 
