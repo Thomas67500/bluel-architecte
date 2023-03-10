25804c14ced6b3ef774a7846685364febe2ceb71
Thomas V <thomas.vog67@gmail.com>
2023-03-11 00:55:12 +0100

    ajout boutons Obj,App,Hotel

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,60 +1,38 @@
  console.log ("connecte !");
 
+
+
+
+
  async function listeCategories () {
  let response= await fetch ("http://localhost:5678/api/categories")
  let infoCategorie = await response.json()
-//infoCategorie.unshift("Tous"); rajoute juste une valeur tous
-/*let tous = {
-    id: 0,
-    name:"tous"                          voulu creer un array tous pour l'inserer dans infoCategorie
-};
-let boutonTous = [tous]; 
-boutonTous.push(infoCategorie);*/
 console.table (infoCategorie);
 
-
 for (let i = 0; i < infoCategorie.length; i++) {
-id = infoCategorie[i].id;
 nom = infoCategorie[i].name;
+id= infoCategorie[i].id;
 affichageBouton(id,nom);
-
-
+}
 
 }
-}
+
 
 listeCategories()
 
 
-
-
-
-
-/*for (let i = 0 ; i < infoImage.length ; i++) {
-    srcValue= infoImage.imageUrl[i]
-    figCaptionValue= infoImage.title[i]
-
-} */
-
 function affichageBouton (id,nom) {
 
-let bouton = document.createElement("bouton");
-let textebouton = document.createElement("texte");
+let position = document.querySelector(".gallery");
+let btn= document.createElement("button");
+let textebouton = document.createElement("span");
 
+btn.setAttribute("li",id);
+console.log(btn);
 
-bouton.setAttribute("btn",id);
-bouton.style.backgroundColor="red";
-
-
-textebouton.append(bouton);
-textebouton.append(nom);
-bouton.append(id);
-console.log(textebouton);
-console.log(id);
-console.log(bouton);
-
-
-
+textebouton.textContent =nom;
+btn.append(textebouton);
+position.append(btn);
 }
 
 
@@ -66,11 +44,12 @@ async function listeProjet() {
 
     let response = await fetch("http://localhost:5678/api/works")
     let infoImage = await response.json()
-        //console.table (infoImage);
+        console.table (infoImage);
     for (let i = 0; i < infoImage.length; i++) {
         srcValue = infoImage[i].imageUrl;
         figCaptionValue = infoImage[i].title;
         affichageProjet(srcValue, figCaptionValue);
+        
     }
 }
 
@@ -95,6 +74,10 @@ function affichageProjet(srcValue, figCaptionValue) {
 
 
 }
+
+
+
+
 
 
 
