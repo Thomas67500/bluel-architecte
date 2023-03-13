926f01048d1911dafaae307e31b613d0a01005c6
Thomas V <thomas.vog67@gmail.com>
2023-03-14 00:47:46 +0100

    affichage bouton tous, debut css et filtre

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,21 +1,14 @@
- console.log ("connecte !");
-
-
-
-
 
  async function listeCategories () {
  let response= await fetch ("http://localhost:5678/api/categories")
  let infoCategorie = await response.json()
 console.table (infoCategorie);
-
+affichageBouton(0,"Tous"); 
 for (let i = 0; i < infoCategorie.length; i++) {
 nom = infoCategorie[i].name;
 id= infoCategorie[i].id;
 affichageBouton(id,nom);
-}
-
-}
+}}
 
 
 listeCategories()
@@ -25,16 +18,29 @@ function affichageBouton (id,nom) {
 
 let position = document.querySelector(".gallery");
 let btn= document.createElement("button");
-let textebouton = document.createElement("span");
-
-btn.setAttribute("li",id);
+btn.setAttribute("class","bouton"+ id);
 console.log(btn);
-
-textebouton.textContent =nom;
-btn.append(textebouton);
+btn.innerText =nom;
 position.append(btn);
+
+/*btn.style.fontFamily="Syne";
+btn.style.fontWeight="700";
+btn.style.fontSize="16px";
+btn.style.textAlign="Center";
+
+btn.style.height="37px";
+btn.style.width="198px";
+btn.style.border="1px";
+btn.style.borderColor="#1D6154";
+btn.style.borderRadius="60px";*/
+
+
+//btn.style.backgroundColor="white";
 }
 
+//let test = document.getElementsByClassName("bouton2");
+//test.style.backgroundColor="white";                                  undefined
+//console.log(test);
 
 
 
@@ -48,7 +54,9 @@ async function listeProjet() {
     for (let i = 0; i < infoImage.length; i++) {
         srcValue = infoImage[i].imageUrl;
         figCaptionValue = infoImage[i].title;
-        affichageProjet(srcValue, figCaptionValue);
+        idCategorie= infoImage[i].categoryId;
+        affichageProjet(srcValue, figCaptionValue,idCategorie);
+        
         
     }
 }
@@ -57,16 +65,17 @@ async function listeProjet() {
 listeProjet()
 
 
-function affichageProjet(srcValue, figCaptionValue) {
+function affichageProjet(srcValue, figCaptionValue,idCategorie) {
 
     let gallery = document.querySelector(".gallery");
+    console.log(gallery);
 
     let figure = document.createElement("figure");
     let img = document.createElement("img");
     let figCaption = document.createElement("figCaption");
 
    
-    
+    figure.setAttribute("class",idCategorie);
     img.setAttribute("src",srcValue);
     figCaption.textContent = figCaptionValue;
     gallery.append(figure);
@@ -75,7 +84,9 @@ function affichageProjet(srcValue, figCaptionValue) {
 
 }
 
-
+//let boutonTous = document.querySelector(".bouton0");
+//boutonTous.onclick = () => {
+//};
 
 
 
