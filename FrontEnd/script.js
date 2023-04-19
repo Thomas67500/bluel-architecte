6f7517ee06b95e0bb197fa6560cbde62323aeb81
Thomas V <thomas.vog67@gmail.com>
2023-04-20 00:25:08 +0200

    MAJ

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,24 +1,26 @@
 let urlApi = "http://localhost:5678/api/"
 let token = sessionStorage.getItem("token")
-//console.log(token);
 
+
+// permet de lier l'id et le nom des boutons aux boutons d'affichageBouton
+//creation du bouton tous
 
  async function listeCategories () {
  let response= await fetch ("http://localhost:5678/api/categories")
  let infoCategorie = await response.json()
-//console.table (infoCategorie);
-let position = document.querySelector(".gallery");
-let portfolio=document.querySelector("#portfolio");
+  let position = document.querySelector(".gallery");
+  let portfolio=document.querySelector("#portfolio");
 let blocBouton= document.createElement("div");
 blocBouton.setAttribute("class","bloc-bouton");
-let btn = affichageBouton(0,"Tous"); 
-blocBouton.append(btn);
-portfolio.insertBefore(blocBouton,position); 
+   let btn = affichageBouton(0,"Tous"); 
+   blocBouton.append(btn);
+   portfolio.insertBefore(blocBouton,position); 
 for (let i = 0; i < infoCategorie.length; i++) {
 nom = infoCategorie[i].name;
 id= infoCategorie[i].id;
 let btn=affichageBouton(id,nom);
 blocBouton.append(btn);
+
 portfolio.insertBefore(blocBouton,position); 
 if (sessionStorage.getItem("token")){
 blocBouton.style.display="none"
@@ -26,36 +28,26 @@ blocBouton.style.display="none"
 }
 }}
 
-
 listeCategories()
+
 
 // afficher les boutons sur l'index
 function affichageBouton (id,nom) {
-
-
 let btn= document.createElement("button");
 btn.setAttribute("class","bouton");
-//console.log(btn);
 btn.innerText =nom;
 btn.addEventListener("click",function(){
-
- listeProjet(id)
- 
+  listeProjet(id)
 })
-return btn;
+  return btn;
 }
 
-
-
-
-
-
-
+ //recuperation du works, si la categoryID est identique au idCategorie,on recupere le srcvalue ainsi que sa figcaption, qui va ensuite etre utiliser dans affichageProjet
+ // si correpond a rien, correspond a zero est sera utiliser pour le bouton Tous
 async function listeProjet(idCategorie=0) {
 
     let response = await fetch("http://localhost:5678/api/works")
     let infoProjet = await response.json()
-    //console.table(infoProjet);
         let gallery = document.querySelector(".gallery")
         
         nettoyage(gallery)
@@ -71,7 +63,6 @@ async function listeProjet(idCategorie=0) {
         
     }
      else if(idCategorie==0){
-       //console.log("idCategorie"+idCategorie)
         srcValue = infoProjet[i].imageUrl;
         figCaptionValue = infoProjet[i].title;
         idValue = infoProjet[i].id;
@@ -83,17 +74,15 @@ async function listeProjet(idCategorie=0) {
     }
 }
 
- // affichage des images dans la premiere modal //
+ //identique a listeProjet mais sera utiliser pour la premiere modal
 async function listeProjetModal(idCategorie=0) {
     let response = await fetch("http://localhost:5678/api/works")
     let infoProjet = await response.json()
-    //console.table(infoProjet);
     let GalerieImage= document.createElement("div");
     GalerieImage.setAttribute("class","galerie-image");
     
         
-        
-
+    
     for (let i = 0; i < infoProjet.length; i++) {
         if(infoProjet[i].categoryId==idCategorie){
 
@@ -104,7 +93,6 @@ async function listeProjetModal(idCategorie=0) {
         affichageGalerie(srcValue);
     }
      else if(idCategorie==0){
-        //console.log("idCategorie"+idCategorie)
         srcValue = infoProjet[i].imageUrl;
         figCaptionValue = infoProjet[i].title;
         idValue = infoProjet[i].id;
@@ -115,35 +103,25 @@ async function listeProjetModal(idCategorie=0) {
     }
 }
 
-listeProjetModal(0)
+    listeProjetModal(0)
+    listeProjet(0)
 
-    
-
-
-
-listeProjet(0)
-
-
+//permet l'affichage des projets de façon dynamique sur l'index
 function affichageProjet(srcValue, figCaptionValue) {
 
     let gallery = document.querySelector(".gallery");
-    //console.log(gallery);
-
     let figure = document.createElement("figure");
     let img = document.createElement("img");
     let figCaption = document.createElement("figCaption");
-
-   
 
     img.setAttribute("src",srcValue);
     figCaption.textContent = figCaptionValue;
     gallery.append(figure);
     figure.append(img, figCaption);
     
-
-
 }
 
+//permet d'enlever un element sur la page
 function nettoyage(element) {
     while (element.firstChild) {
         element.removeChild(element.firstChild);
@@ -153,8 +131,8 @@ function nettoyage(element) {
 
 
 
+ // permet l'ouverture de la premiere modal 
 let modal = null
-
 const openModal = function (e) {
 e.preventDefault()
 const GallerieModal= document.querySelector(".galerie-image")
@@ -170,10 +148,11 @@ modal =target
 modal.addEventListener("click", closeModal)
 modal.querySelector(".js-modal-close").addEventListener("click",closeModal)
 modal.querySelector(".js-modal-stop").addEventListener("click",stopPropagation)
-//modal = null
 
 }
 
+
+//fermeture de la premiere modal
 const closeModal = function (e) {
     const modal = document.querySelector("#modal1")   
     if  (modal === null) return
@@ -188,17 +167,19 @@ modal.querySelector(".js-modal-stop").removeEventListener("click",stopPropagatio
 //modal = null
 }
 
+//evite la fermeture des modales quand on clique n'importe ou
 const stopPropagation = function (e) {
      e.stopPropagation()
 }
 
+//ouvre la modal 1
 const modal1 =document.querySelector(".js-modal")
 modal1.addEventListener("click",openModal)
 
 
 
 
-
+//permet l'ouverture de la modal2 
 const openModal2 = function(e){
     e.preventDefault()
     initModal2()
@@ -215,6 +196,7 @@ modal.querySelector(".js-modal-stop2").addEventListener("click",stopPropagation)
 //modal = null
 }
 
+//ferme la modal 2
 const closeModal2 = function (e) {
     const modal2 = document.querySelector("#modal2")   
     if  (modal2 === null) return
@@ -224,12 +206,10 @@ modal2.style.display = "none"
 modal2.setAttribute("aria-hidden","true")
 modal2.removeAttribute("aria-modal")
 modal2.removeEventListener("click", closeModal2)
-//modal.querySelector(".js-modal-close").removeEventListener("click", closeModal2)
-//modal2.querySelector(".js-modal-stop").removeEventListener("click",stopPropagation)
-//modal2 = null
+
 }
 
-
+// ouvre la modal2
 const modal2 = document.querySelector(".js-modal2")
     modal2.addEventListener("click",openModal2)
 
@@ -243,15 +223,11 @@ retourModal1.addEventListener("click",openModal)
 
 
 
-// java input// 
+// afficher l'image dans la modal2 //
 const input = document.querySelector("input")
 const output = document.querySelector("output")
 const imagePreview = document.querySelector(".test")
 let imagesArray = []
-
-
-
-// afficher l'image//
 const boutonUpload= document.getElementById("label-upload")
     const texteUpload= document.querySelector(".texte-upload")
 
@@ -260,20 +236,15 @@ function displayImages(image) {
         images = `<div class="image">
                 <img src="${URL.createObjectURL(image)}" alt="image">
               </div>`
-
     output.innerHTML = images
    if ((output.innnerHTML= images)) {
      boutonUpload.style.display="none"
      texteUpload.style.display="none"
-    }
-    
+    } 
   }
-
-
 input.addEventListener("change", function() {
 const file= input.files
 displayImages(file[0])
-  
 })
 
  // creation const pour index modifier//
@@ -311,7 +282,7 @@ loginIndex()
 let GalerieImage= document.createElement("div");
 GalerieImage.setAttribute("class","galerie-image");
 
-// affichage des images dans la modale
+// affichage des images dans la modale 1
 function affichageGalerie(srcValue,idValue){
     
     let titreModal = document.querySelector(".titre-modal");
@@ -457,9 +428,7 @@ form.addEventListener("submit",function(e){
 
 let formData =new FormData(form);
 
-//console.log([...formData,])
 formData.append("title",titre.value);
-// formData.append("image",image.files[0]);
 formData.append("category",categories.value);
 
 fetch("http://localhost:5678/api/works",{
@@ -469,12 +438,13 @@ body: formData,
 })
 .then (res => res.json())
 .then (res=> {
-    //console.log(res)
     listeProjet(0)
     closeModal2(e)
  
 })
 })
+
+
 
 function initModal2() {
 
