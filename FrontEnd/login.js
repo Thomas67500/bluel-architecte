e4e98cc04e5956f306089fc6be267b88a5abea5c
Thomas V <thomas.vog67@gmail.com>
2023-04-21 16:06:24 +0200

    MAJ et finition de la page login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -1,60 +1,55 @@
 
 const form = document.getElementById("form");
 
-form.addEventListener("submit", e =>{
-e.preventDefault();
-const error = document.querySelector(".error");
- error.style.display ="none";
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    const error = document.querySelector(".error");
+    error.style.display = "none";
 
-const email = document.getElementById("email").value;
-const password = document.getElementById("pass").value;
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("pass").value;
 
-  if ((email ==="") || (password ==="")) {
-    error.innerHTML="Nom d'utilisateur ou mot de passe erroné";
-    error.style.display="block";
+    if ((email === "") || (password === "")) {
+        error.innerHTML = "Nom d'utilisateur et/ou mot de passe sont obligatoire";
+        error.style.display = "block";
+    } else {
 
-  } 
-  else
-  {
+        fetch("http://localhost:5678/api/users/login", {
+                method: "POST",
+                headers: { "Content-type": "application/json" },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                }),
+            })
+            .then(res => res.json())
+            .then((data) => {
+                if (data.token != undefined) {
+                    window.sessionStorage.setItem("token", data.token);
+                    const tokenSeul = sessionStorage.getItem("token");
+                    console.log(tokenSeul);
+                    location.href = "index.html";
+                } else if (data.message != undefined) {
 
+                    error.innerHTML = data.message
+                    error.style.display = "block";
 
-fetch("http://localhost:5678/api/users/login",{
-method:"POST",
-headers : {"Content-type":"application/json"},
-body:JSON.stringify({
-    email : email,
-    password : password,
-}),
-})
-.then (res => res.json())
-.then ((data) => {
-  if (data.token != undefined){
-  window.sessionStorage.setItem("token",data.token);
-   const tokenSeul = sessionStorage.getItem("token");
-   console.log(tokenSeul);
-   location.href = "index.html";
-  }
+                } else {
 
- else if (data.message != undefined){
-
- error.innerHTML= data.message
- error.style.display= "block"; 
-
- }
- else {
-
-   error.innerHTML="Nom d'utilisateur ou mot de passe erroné";
-   error.style.display="block";
- 
- 
- 
-}
-
-})
+                    error.innerHTML = "Nom d'utilisateur ou mot de passe erroné";
+                    error.style.display = "block";
 
 
 
-);
+                }
+
+            })
+
+
+    }
+
+});
+
 
 
 
